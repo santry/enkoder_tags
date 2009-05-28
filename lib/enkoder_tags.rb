877b45cbd9a1f944f7d4d04851bf229f8b4ffdcb
Andrew Neil <andrew.jr.neil@gmail.com>
2009-05-28 16:13:20 +0100

    Added descriptions for enkode and enkode_mailto.

--- lib/enkoder_tags.rb
+++ lib/enkoder_tags.rb
@@ -3,10 +3,42 @@ module EnkoderTags
 
   class TagError < StandardError; end
 
+  desc %{
+    Everything within <r:enkode></r:enkode> tags will be obfuscated using 
+    javascript.
+    
+    Be aware that obfuscated content will not be visible to web clients 
+    which do not have javascript enabled.
+  }
   tag "enkode" do |tag|
     Enkoder.new(tag.expand).enkode
   end
 
+  desc %{
+    The enkode_mailto tag obfuscates mailto links. This makes it very 
+    difficult for bots to harvest email addresses, allowing you to publish 
+    email addresses on the web without attracting spam.
+    
+    The enkode_mailto tag accepts the following attributes as tags:
+    
+    * @email@ (_manadatory_) - the email address you want to publish
+    * @link_text@ - The text which will display in the link 
+      (if left blank, email address is used)
+    * @title_text@ - Populates the @title@ attribute
+    * @subject@ - Text entered here will be used as the _Subject_ for emails
+    
+    Any additional attributes (e.g. @class@ or @id@) provided will also be 
+    applied to the tag.
+    
+    *Usage:*
+
+    <pre><code><r:enkode_mailto email="hello@example.com" link_text="Say hello"
+    title_text="Click here to say hello by email" subject="Hello" />
+    </code></pre>
+    
+    Be aware that browsers which do not have javascript enabled will not see 
+    the email address.
+  }
   tag "enkode_mailto" do |tag|
     attr = tag.attr.symbolize_keys
     
